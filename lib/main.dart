b8befd7b89ab8120f318e992cc2324124e83b406
DIEGO_SEBASTIAN_LARA_BAEZA <a.21308051280926@cbtis128.edu.mx>
2024-04-25 18:08:06 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Flutter demo",
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ropa leFleur* LARA 0312'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/camisa.webp',
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/gorro.webp',
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/pantalon.webp',
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/teni1.webp',
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/teni2.webp',
+    'https://raw.githubusercontent.com/LaraD128/img_iOS/main/golf/teni3.webp',
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
+        backgroundColor: Color(0xffead76e),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        color: Color(0xfffbfdf3),
+        child: Center(
+          child: Column(
+            children: [
+              CarouselSlider(
+                items: imgList
+                    .map((e) => Center(
+                          child: Image.network(e),
+                        ))
+                    .toList(),
+                options: CarouselOptions(
+                  initialPage:
+                      _currentPage, // Asegura que la página inicial sea la actual.
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, _) {
+                    setState(() {
+                      _currentPage = index; // Actualiza la página actual.
+                    });
+                  },
+                ),
+              ),
+              buildCarouselIndicator()
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < imgList.length; i++)
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: i == _currentPage ? 7 : 5,
+            width: i == _currentPage ? 7 : 5,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: i == _currentPage ? Colors.black : Colors.grey,
+            ),
+          )
+      ],
     );
   }
 }
